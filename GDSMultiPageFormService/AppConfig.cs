c1ea771abebae911e25cee097f1475da6ba6b8ad
OluwatobiAwe <oluwatobi.awe@hee.nhs.uk>
2023-01-09 13:20:17 +0000

    TD-878 Redis support

---
+++ GDSMultiPageFormService/AppConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GDS.MultiPageFormData
+{
+    public static class AppConfig
+    {
+        public static bool GetMultiPageFormDataStore()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"), optional: false)
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json"), optional: false)
+                .Build();
+            try
+            {
+                return configuration.GetSection("MultiPageFormService").GetSection("Database").Value.ToLower().Equals("redis");
+            }
+            catch { return false; }
+        }
+    }
+}
